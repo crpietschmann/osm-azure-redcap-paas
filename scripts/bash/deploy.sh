c1957cf6968c4690585891d3c133f5892537a8d7
Chris Pietschmann <chris@build5nines.com>
2025-03-28 14:04:56 -0400

    Update deploy.sh

--- scripts/bash/deploy.sh
+++ scripts/bash/deploy.sh
@@ -34,7 +34,7 @@ echo "extension=/usr/local/lib/php/extensions/no-debug-non-zts-20220829/mysqli.s
 echo "Testing DNS"
 nslookup "$APPSETTING_StorageAccount.blob.core.windows.net"
 
-redcapZipPath="/site/redcap.zip"
+redcapZipPath="~/site/redcap.zip"
 
 cd /tmp
 if [ -z "$APPSETTING_redcapAppZip" ]; then
